925e065db2f465c43d0ab5cabb87795427f95314
skrapyrobot <skrapyrobot@outlook.com>
2025-06-14 16:05:21 -0600

    full-stack-06-42

--- react/react-essentials/src/App.tsx
+++ react/react-essentials/src/App.tsx
@@ -1,41 +1,10 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <Header />
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <button onClick={() => setCount((count) => count - 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
@@ -44,17 +13,33 @@ export default App;
 
 export function Header() {
     return (
-        <header>
-            <img src={reactLogo} alt="React icon: A fancy atom" />
-            <h1>React Essentials</h1>
-            <p>
-                These are fundamental{' '}
+        <>
+            <header>
                 <a href="https://react.dev" target="_blank">
-                    React
-                </a>{' '}
-                concepts you will need for almost any app you are going to
-                build!{' '}
-            </p>
-        </header>
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+                <h1>React Essentials</h1>
+                <p>
+                    These are{' '}
+                    {reactDescriptions[genRandomInt(reactDescriptions.length)]}{' '}
+                    <a href="https://react.dev" target="_blank">
+                        React
+                    </a>{' '}
+                    concepts you will need for almost any app you are going to
+                    build!{' '}
+                </p>
+            </header>
+        </>
     );
+}
+
+const reactDescriptions = ['fundamental', 'crucial', 'core'];
+
+function genRandomInt(max: number) {
+    const randomInteger = Math.floor(Math.random() * max);
+    return randomInteger;
 }
