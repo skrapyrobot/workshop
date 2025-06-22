d91f2e0050fac6cfc8f629a5fb0ebfffb9dc0355
robot <skrapyrobot@outlook.com>
2025-06-22 08:41:09 -0600

    Removed TODOs

--- rustlings/exercises/02_functions/functions1.rs
+++ rustlings/exercises/02_functions/functions1.rs
@@ -1,7 +1,4 @@
-// TODO: Add some function with the name `call_me` without arguments or a return value.
-pub fn call_me() {
-    //
-}
+pub fn call_me() {}
 
 fn main() {
     call_me(); // Don't change this line
