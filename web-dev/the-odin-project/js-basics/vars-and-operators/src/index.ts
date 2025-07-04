f0dca15c8f7db8b599c1c09a7a1e90cc37f2aac1
skrapyrobot <skrapyrobot@outlook.com>
2025-07-03 19:35:09 -0600

    vars-and-operators

---
+++ web-dev/the-odin-project/js-basics/vars-and-operators/src/index.ts
@@ -0,0 +1,15 @@
+console.log('Hello there');
+
+const person_name = 'John';
+const person_surname = 'Doe';
+
+console.log(`Hellow there ${person_name} ${person_surname}`);
+
+let person_age = 11;
+console.log(person_age);
+person_age = 54;
+console.log(person_age);
+
+const pi = 3.14;
+
+console.log(typeof pi);
