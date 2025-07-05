395f899329977ba2567605c23841bf54fc5d9ede
skrapyrobot <skrapyrobot@outlook.com>
2025-07-04 21:26:08 -0600

    function basics

--- web-dev/the-odin-project/js-basics/vars-and-operators/src/index.ts
+++ web-dev/the-odin-project/js-basics/vars-and-operators/src/index.ts
@@ -1,15 +1,28 @@
-console.log('Hello there');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Hello there');
 
-const person_name = 'John';
-const person_surname = 'Doe';
+    const person_name = 'John';
+    const person_surname = 'Doe';
 
-console.log(`Hellow there ${person_name} ${person_surname}`);
+    console.log(`Hellow there ${person_name} ${person_surname}`);
 
-let person_age = 11;
-console.log(person_age);
-person_age = 54;
-console.log(person_age);
+    let person_age = 11;
+    console.log(person_age);
+    person_age = 54;
+    console.log(person_age);
 
-const pi = 3.14;
+    const pi = 3.14;
 
-console.log(typeof pi);
+    console.log(typeof pi);
+
+    // const text = 'Hellow there robot!';
+
+    const test_text = 'Replacement text';
+    const paragraph = document.getElementById('demo');
+
+    if (paragraph != null) {
+        paragraph.innerHTML = test_text;
+    } else {
+        console.log('Paragraph not found!');
+    }
+});
